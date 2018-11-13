16a9d8c192da63fc6e77c79198686f2dbd64e4de
spierre95 <stephen.pierre20@gmail.com>
2018-11-13 15:18:01 -0500

    styled recipe cards

--- src/components/recipe/RecipeCard.js
+++ src/components/recipe/RecipeCard.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const RecipeCard = (props) => {
+export const RecipeCard = props => {
+  return (
+    <div className="recipe-card">
+     <Link to="/recipe/1">
+      <div
+        class="recipe-card__img"
+        style={{ backgroundImage: `url(${props.img})` }}
+        alt={props.title}
+      />
+        <h3 className="heading-tertiary">{props.title}</h3>
+      </Link>
+    </div>
+  );
+};
 
-    return (
-        <div className="recipe-card">
-            <div className="recipe-card__box">
-                <h2 className="heading-tertiary">{props.title}</h2>
-                <img src={props.img} alt={props.title} class="recipe-card__img"/>
-            </div>
-        </div>
-    )
-
-}
-
-export default RecipeCard
+export default RecipeCard;
--- src/components/recipe/RecipeList.js
+++ src/components/recipe/RecipeList.js
@@ -4,16 +4,11 @@ import { RecipeCard } from 'components/recipe/RecipeCard';
 
 export class RecipeList extends Component {
 
-  UNSAFE_componentWillReceiveProps(nextProps){
-    console.log(nextProps);
-  }
-
   render() {
     const { recipes } = this.props
-    if(!recipes) return null
       return(
            <div className="search-results__container">
-            {recipes.map(recipe =>
+            {recipes && recipes.map(recipe =>
                 <RecipeCard img={recipe.image_url} id={recipe.recipe_id} title={recipe.title}/>
             )}
           </div>
