cc2dd7d459b3d26aff9cdf7803903b819ef064c2
spierre95 <stephen.pierre20@gmail.com>
2018-11-23 19:04:15 -0500

    tsting deployment

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ import Header from "containers/layout/Header";
 import DisplayRecipes from "containers/DisplayRecipes";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "reducers";
+import rootReducer from "reducers/root";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
